586394e17462e7ffc0a9a90d7f1fc8bf8cdeb8bb
TAWilliamson-Dev <travis.williamson@snhu.edu>
2026-04-19 22:44:33 -0500

    module 5 changes pt 2
    Additional changes, bringing project in line with module 5

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Trip = require('../models/travlr');
 const Model = mongoose.model('trips');
 
-const tripsList = async(requestAnimationFrame, res) => {
+const tripsList = async(req, res) => {
     const q = await Model.find({}).exec();
 
     if(!q) {
@@ -13,6 +13,18 @@ const tripsList = async(requestAnimationFrame, res) => {
     }
 };
 
+const tripsFindByCode = async(req, res) => {
+    const q = await Model.find({'code' : req.params.tripCode}).exec();
+
+    if(!q) {
+        return res.status(404).json(err);
+    }
+    else {
+        return res.status(200).json(q);
+    }
+}
+
 module.exports = {
-    tripsList
+    tripsList,
+    tripsFindByCode
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,4 +5,6 @@ const tripsController = require("../controllers/trips");
 
 router.route("/trips").get(tripsController.tripsList);
 
+router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
+
 module.exports = router;
